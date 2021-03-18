d2393914b84f5eeda307bf16e3e9c77d96487162
devansh agarwal <manishyoyo9359@gmail.com>
2021-03-18 11:36:04 +0545

    Task Done

--- app/src/main/java/com/example/activitylifecycleandstate/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycleandstate/MainActivity.java
@@ -8,50 +8,35 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName() ;
-    public static final String EXTRA_MESSAGE = "me.mahakagg.twoactivities.extra.MESSAGE";
-    private EditText mMessageEditText;
-    public static final int TEXT_REQUEST = 1;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    private TextView countTextView;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        get the views from their IDs
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
-    }
-
-    public void launchSecondActivity(View view) {
-//        add message to debug logs
-        Log.d(LOG_TAG, "Button Clicked!");
-//        create intent, get string from EditText, put it in intent extras and send to SecondActivity
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-//       receive result back from SecondActivity
-        startActivityForResult(intent, TEXT_REQUEST);
-    }
-
-    //    method callback when SecondActivity returns result
-//    request code is the one used in startActivityForResult(); resultCode is the code which is received (can be RESULT_OK or RESULT_CANCELED; data is the intent which carries the data
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST){
-            if (resultCode == RESULT_OK){
-//                get the reply message from SecondActivity, put the information in TextView and make them visible
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-            }
+        countTextView = findViewById(R.id.count_textView);
+        if (savedInstanceState != null){
+            String i = savedInstanceState.getString("count_value");
+            countTextView.setText(i);
+            count = Integer.parseInt(i);
         }
+    }
+
+    public void increment(View view) {
+        count ++;
+        countTextView.setText(String.valueOf(count));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("count_value", countTextView.getText().toString());
     }
 }
